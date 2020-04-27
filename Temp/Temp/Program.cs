2cae773a85d3be40636525147a8c136cc398fc32
batzeliosthanos <thmpatzelios@athtech.gr>
2020-04-27 13:14:13 +0300

    first com

---
+++ Temp/Temp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
